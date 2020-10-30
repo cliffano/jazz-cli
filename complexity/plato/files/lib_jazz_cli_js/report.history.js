f49cc8f48f23eac017e4d93759b821b5b9e662b1
cliffano <cliffano@users.noreply.github.com>
2020-10-30 09:23:43 +0000

    deploy: c3e7234cef336d7e0f05145fac172dacad635f92

--- complexity/plato/files/lib_jazz_cli_js/report.history.js
+++ complexity/plato/files/lib_jazz_cli_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 30 Oct 2020 09:23:33 GMT","sloc":32,"lloc":7,"functions":2,"deliveredBugs":0.107,"difficulty":8.13,"maintainability":79.955,"lintErrors":3}]
+__history = [{"date":"Fri, 30 Oct 2020 09:23:37 GMT","sloc":32,"lloc":7,"functions":2,"deliveredBugs":0.107,"difficulty":8.13,"maintainability":79.955,"lintErrors":3}]
